65bcc08e1696be581427da0594a7ed74702627f5
Lisa Brennan <lisa.brennan@lilly.com>
2026-08-10 13:42:09 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-pip install -r backend\requirements.txt
-start /min cmd /c "uvicorn backend.main:app --host 127.0.0.1 --port 8000"
-timeout /t 2 >nul
-start http://127.0.0.1:8000
